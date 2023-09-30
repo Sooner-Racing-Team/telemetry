2bff540501369a94a8e69d1a117de1234bd183e1
Barrett Ray <onkoe@protonmail.com>
2023-09-30 13:03:39 -0500

    chore: Add testing code to send

--- send/src/main.rs
+++ send/src/main.rs
@@ -1,4 +1,4 @@
-#![no_std]
+#![no_std]   
 #![no_main]
 
 use panic_halt as _;
@@ -7,21 +7,22 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600); // connect to serial
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    ufmt::uwriteln!(serial, "Hello there!\r").unwrap();
+
+    println!("this is a std lib thing, and wont work here :(");
 
     let mut led = pins.d13.into_output();
 
     loop {
         led.toggle();
         arduino_hal::delay_ms(1000);
-    }
+        ufmt::uwriteln!(serial, "f(4) = {}", f(4));
+    }   
+}
+
+
+fn f(x: i64) -> i64 {
+    i64::pow(x, 2)
 }
